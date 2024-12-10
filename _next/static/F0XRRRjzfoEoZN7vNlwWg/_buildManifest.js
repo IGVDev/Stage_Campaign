9bfdde73dbb3029706c67ad73dd01778be8a04ec
IGVDev <46609359+IGVDev@users.noreply.github.com>
2024-12-10 13:18:57 +0000

    Deploying to gh-pages from @ IGVDev/Stage_Campaign@568679594a9e3f46e0c4507eac953b1903f6bad9 🚀

--- _next/static/F0XRRRjzfoEoZN7vNlwWg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/538-0f1df3235562758a.js","static/chunks/pages/index-3ba67b17fb8b875f.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
