3ddb710294f82de6463407d3172202eb18c8e33f
IGVDev <46609359+IGVDev@users.noreply.github.com>
2024-12-10 12:47:56 +0000

    Deploying to gh-pages from @ IGVDev/Stage_Campaign@026af696c512cbec380b7420d412264f9cd43bf6 🚀

--- _next/static/hblpyTmeZsJDykIHAI5Xj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/538-ae30475f24369431.js","static/chunks/pages/index-4f880b7a7c27d26e.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/xBoVrKHqJEYV5mjXwR0p9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/538-0f1df3235562758a.js","static/chunks/pages/index-4f880b7a7c27d26e.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
